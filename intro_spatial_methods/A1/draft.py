734157f839fbad24556f3e4a9e2750656dbed0df
KazuichiroTaira <ktaira098@gmail.com>
2021-09-24 22:45:14 +0300

    dekitara ureshii

---
+++ intro_spatial_methods/A1/draft.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import KDTree
+
+from IDW import read_laserdata, read_image, read_camera_orientation_info
+
+
+def main():
+    laser_data = read_laserdata()
+    laser_x = laser_data[:, 0]
+    laser_y = laser_data[:, 1]
+    laser_z = laser_data[:, 2]
+    laser_xy = laser_data[:, :2]
+    print("from Laser data")
+    print(f'x-coordinate: min={laser_x.min()}, max={laser_x.max()}')
+    print(f'y-coordinate: min={laser_y.min()}, max={laser_y.max()}')
+    orthophoto = read_image()
+
+    print("read camera orientation")
+    pp_x, pp_y, c, r, x_0, y_0, z_0 = read_camera_orientation_info()
+    print(f'pp_x = {pp_x}, pp_y = {pp_y}, c = {c}, r = {r}, x_0 = {x_0}, y_0 = {y_0}, z_0 = {z_0}')
+    print("*" * 30)
+    tree = KDTree(laser_xy)
+
+    n_pixel_rows, n_pixel_columns = orthophoto.shape
+
+    x_start = 6111.4
+    y_start = 5267.0
+    grid_size = 0.2
+
+    pixel_x_start = x_start
+    pixel_y_start = y_start - n_pixel_rows*grid_size
+
+    pixel_x_stop = x_start + n_pixel_columns*grid_size
+    pixel_y_stop = y_start
+
+    laser_x_start = laser_x.min()  # 6088.331
+    laser_y_start = laser_y.min()
+
+    laser_x_stop = laser_x.max()
+    laser_y_stop = laser_y.max()
+
+    print(
+        f"pixel_x_start = {pixel_x_start}\n"
+        f"pixel_y_start = {pixel_y_start}\n\n"
+        f"pixel_x_stop = {pixel_x_stop}\n"
+        f"pixel_y_stop = {pixel_y_stop}\n\n"
+        f"laser_x_start = {laser_x_start}\n"
+        f"laser_y_start = {laser_y_start}\n\n"
+        f"laser_x_stop = {laser_x_stop}\n"
+        f"laser_y_stop = {laser_y_stop}\n\n"
+    )
+
+    # x_start = laser_x.min()  # 6088.331
+    # x_end = laser_x.max()    # 6129.6
+    # len_x = x_end - x_end
+    # x_points = len_x / grid_size
+    # n_x_cod = int(x_points)
+    #
+    # y_start = laser_y.min()  # 5226.271
+    # y_end = laser_y.max()    # 5276.468
+    # len_y = y_end - y_end
+    # y_points = len_y / grid_size
+    # n_y_cod = int(y_points)
+
+    # print("*" * 100)
+    #
+    # pixel_x_start = x_start
+    # pixel_y_start = y_start # - n_pixel_rows*grid_size
+    #
+    # pixel_x_stop = x_start + n_pixel_columns*grid_size
+    # pixel_y_stop = y_start + n_pixel_rows*grid_size
+    #
+    # laser_x_start = laser_x.min()  # 6088.331
+    # laser_y_start = laser_y.min()
+    #
+    # laser_x_stop = laser_x.max()
+    # laser_y_stop = laser_y.max()
+    #
+    # print(
+    #     f"pixel_x_start = {pixel_x_start}\n"
+    #     f"pixel_y_start = {pixel_y_start}\n\n"
+    #     f"pixel_x_stop = {pixel_x_stop}\n"
+    #     f"pixel_y_stop = {pixel_y_stop}\n\n"
+    #     f"laser_x_start = {laser_x_start}\n"
+    #     f"laser_y_start = {laser_y_start}\n\n"
+    #     f"laser_x_stop = {laser_x_stop}\n"
+    #     f"laser_y_stop = {laser_y_stop}\n\n"
+    # )
+
+
+main()
---
+++ intro_spatial_methods/A1/draft2.py
@@ -0,0 +1,177 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import KDTree
+
+
+"""
+loading data
+"""
+
+
+def read_camera_orientation_info(filename="camera_orientation_info.txt"):
+
+    with open(filename, 'r') as f:
+        lines = f.read().splitlines()
+
+    pp_x = float(lines[1])
+    pp_y = float(lines[2])
+    c = float(lines[4])
+
+    x_0 = float(lines[9])
+    y_0 = float(lines[10])
+    z_0 = float(lines[11])
+
+    r = np.zeros((3, 3))
+
+    for i in range(3):
+        str_row = lines[13 + i].split("\t")
+        r[i] = [float(x) for x in str_row]
+
+    return pp_x, pp_y, c, r, x_0, y_0, z_0
+
+
+def read_laserdata(filename="laserdata.txt"):
+    df = pd.read_csv(filename, sep=' ', header=None)
+    laser_data = df.to_numpy()
+    return laser_data
+
+
+def read_image(filename="test_image_gray.tif"):
+    image = plt.imread(filename)
+    return image
+
+
+def display_image(filename="test_image_gray.tif"):
+    image = plt.imread(filename)
+    # n_rows, n_columns = image.shape
+    fig, ax = plt.subplots()
+    ax.imshow(image, cmap='gray', vmin=0, vmax=255)
+    ax.set_axis_off()
+
+    fig.tight_layout(pad=0, h_pad=0, w_pad=0)
+    plt.savefig('image.png', bbox_inches='tight', pad_inches=0)
+    plt.show()
+
+
+def create_orthophoto_fit_lazer(laser_x_min, laser_x_max, laser_y_min, laser_y_max, grid_size):
+
+    len_x = laser_x_max - laser_x_min
+    x_points = len_x / grid_size
+    x_points_as_row = math.ceil(x_points)
+
+    len_y = laser_y_max - laser_y_min
+    y_points = len_y / grid_size
+    y_points_as_columns = math.ceil(y_points)
+
+    return x_points_as_row, y_points_as_columns
+
+
+"""
+Computation 
+"""
+
+
+def d(x, x_i):
+
+    try:
+        X, Y = x
+        X_i, Y_i = x_i
+        return np.sqrt((X - X_i) ** 2 + (Y - Y_i) ** 2)
+    except:
+        print('was it testing with 1D?')
+        return np.sqrt((x - x_i) ** 2)
+
+
+def w(d_i, p=2):
+    return 1 / d_i**p
+
+
+def u(neighbours_u, neighbours_d):
+
+    n = len(neighbours_d)
+
+    weights = []
+    for i in range(n):
+        d_i = neighbours_d[i]
+        weight_i = w(d_i)
+        weights.append(weight_i)
+
+        # print(f'weight {i+1}: {weight_i}')
+
+    sum_weights = np.sum(weights)
+
+    total = 0
+    for i in range(n):
+        u_i = neighbours_u[i]
+        w_i = weights[i]
+        to_add = w_i * u_i
+        total += to_add
+
+        # print(f'w{i + 1}u{i+1}: {to_add}')
+
+    result = total / sum_weights
+    return result
+
+
+def main():
+
+    laser_data = read_laserdata()
+    laser_x = laser_data[:, 0]
+    laser_y = laser_data[:, 1]
+    laser_z = laser_data[:, 2]
+    laser_xy = laser_data[:, :2]
+
+    print(f'x-coordinate: min={laser_x.min()}, max={laser_x.max()}')
+    print(f'y-coordinate: min={laser_y.min()}, max={laser_y.max()}')
+
+    laser_x_min = laser_x.min()
+    laser_x_max = laser_x.max()
+    laser_y_min = laser_y.min()
+    laser_y_max = laser_y.max()
+
+    grid_size = 0.2
+
+    x_points_as_row, y_points_as_columns = create_orthophoto_fit_lazer(laser_x_min, laser_x_max, laser_y_min,
+                                                                       laser_y_max, grid_size)
+
+    print(f"row {x_points_as_row}, columns {y_points_as_columns}")
+
+    orthophoto = read_image()
+
+    print("read camera orientation")
+    pp_x, pp_y, c, r, x_0, y_0, z_0 = read_camera_orientation_info()
+    print(f'pp_x = {pp_x}, pp_y = {pp_y}, c = {c}, r = {r}, x_0 = {x_0}, y_0 = {y_0}, z_0 = {z_0}')
+    print("*" * 30)
+
+    tree = KDTree(laser_xy)
+
+    pixels_rows, pixels_columns = x_points_as_row, y_points_as_columns  # use (1, 3) or orthophoto.shape
+    DEM = np.zeros((pixels_rows, pixels_columns))
+
+    x_start = 6111.4
+    y_start = 5267
+    grid_size = 0.2
+
+    for i in range(pixels_rows):
+        for j in range(pixels_columns):
+
+            X = x_start + j*grid_size
+            Y = y_start - i*grid_size
+
+            dist, ind = tree.query(np.array([[X, Y], ]), k=5)
+            # ind/dist shape: 1 x k
+            neighbours_u = laser_z[ind[0]]
+            neighbours_d = dist[0]
+
+            interpolated_height = u(neighbours_d=neighbours_d,
+                                    neighbours_u=neighbours_u)
+            DEM[i, j] = interpolated_height
+
+    print(DEM)
+
+
+if __name__ == "__main__":
+    main()
+
